2d3c105ad1ae780000a492eb70f7133f687c7943
martins <meloyi21@gmail.com>
2023-03-17 16:17:04 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
+/**
+ *main-entry point
+ *return: always 0 (success)
+ */
+int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return 0;
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
